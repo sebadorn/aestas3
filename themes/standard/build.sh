ce0fd61b67d710effdba8c6148511b132ca6c493
sebadorn <seba_dorn@gmx.de>
2015-01-10 02:30:57 +0100

    [Theme] Using LESS. Combine and minify at least my own JavaScript. Build script.

---
+++ themes/standard/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+BASEDIR=$(dirname "$0")
+CLOSURE="/home/$USER/.closure-compiler-v20141215/compiler.jar"
+
+cd "$BASEDIR"
+
+echo ' ----- ----- -----'
+echo ' Building standard theme.'
+
+echo -n ' Compiling LESS to CSS ...'
+lessc -x less/style.less css/style.css
+echo ' Done.'
+
+echo -n ' Combining and minifying JavaScript files ...'
+cat 'js/comment-preview.js' 'js/comment-validate.js' > 'js/tmp.js'
+sed -i 's/"use strict";//' 'js/tmp.js'
+sed -i '1s/^/"use strict";/' 'js/tmp.js'
+
+java -jar "$CLOSURE" --js 'js/tmp.js' --js_output_file 'js/combined.js' --language_in=ECMASCRIPT5_STRICT
+rm 'js/tmp.js'
+echo ' Done.'
+
+echo ' All done.'
+echo ' ----- ----- -----'
--- themes/standard/head.php
+++ themes/standard/head.php
@@ -4,17 +4,14 @@
 	<title><?php echo $title ?></title>
 	<link rel="icon" href="<?php echo THEME_PATH ?>img/favicon-192.png" type="image/png" sizes="192x192" />
 	<link rel="icon" href="<?php echo THEME_PATH ?>img/favicon-32.png" type="image/png" sizes="32x32" />
-	<link rel="stylesheet" href="<?php echo THEME_PATH ?>css/screen.css" />
-	<link rel="stylesheet" href="<?php echo THEME_PATH ?>js/shl3/styles/shCore.css" />
-	<link rel="stylesheet" href="<?php echo THEME_PATH ?>css/shThemeSeba.css" />
+	<link rel="stylesheet" href="<?php echo THEME_PATH ?>css/style.css" />
 	<link rel="alternate" type="application/rss+xml" title="Neue Einträge (RSS)" href="http://feeds2.feedburner.com/sebadorn" />
 	<link rel="alternate" type="application/rss+xml" title="Neue Kommentare (RSS)" href="<?php echo URL ?>feed/comments.php" />
 	<script src="<?php echo THEME_PATH ?>js/shl3/scripts/shCore.js"></script>
 	<script src="<?php echo THEME_PATH ?>js/shl3/scripts/shAutoloader.js"></script>
 <?php if( IS_SINGLE_POST ): ?>
-	<script src="<?php echo THEME_PATH ?>js/md5.min.js"></script>
-	<script src="<?php echo THEME_PATH ?>js/comment-preview.js"></script>
-	<script src="<?php echo THEME_PATH ?>js/comment-validate.js"></script>
+	<script async src="<?php echo THEME_PATH ?>js/md5.min.js"></script>
+	<script async src="<?php echo THEME_PATH ?>js/combined.js"></script>
 <?php endif ?>
 	<script>
 		window.addEventListener( "load", function() {
