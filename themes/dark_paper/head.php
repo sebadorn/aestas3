33ffd424634ce2b3a8d0152f16c6e2fea0fd3fa9
sebadorn <sebadorn+git@posteo.de>
2021-11-06 16:01:16 +0100

    [Theme] dark_paper: Updated highlight.js to v11.3.1.

--- themes/dark_paper/head.php
+++ themes/dark_paper/head.php
@@ -13,7 +13,7 @@
 	<link rel="icon" href="<?php echo THEME_PATH ?>img/favicon-192.png" type="image/png" sizes="192x192" />
 	<link rel="icon" href="<?php echo THEME_PATH ?>img/favicon-32.png" type="image/png" sizes="32x32" />
 	<link rel="stylesheet" href="<?php echo THEME_PATH ?>css/style.css?v=7" />
-	<link rel="stylesheet" href="<?php echo THEME_PATH ?>js/highlight/styles/stackoverflow-dark.css" />
+	<link rel="stylesheet" href="<?php echo THEME_PATH ?>js/highlight/styles/stackoverflow-dark.min.css" />
 	<link rel="alternate" type="application/rss+xml" title="New posts (RSS)" href="<?php echo URL ?>feed/" />
 <?php
 
