944723a51c1439dd0a005068f33b32ff0fe7ab2a
sebadorn <sebadorn+git@posteo.de>
2023-06-01 10:16:07 +0200

    [Theme] dark_paper: Add canonical meta link for posts and pages which are references by ID instead of permalink

--- themes/dark_paper/head.php
+++ themes/dark_paper/head.php
@@ -5,16 +5,26 @@
 	<meta name="robots" content="noindex" />
 <?php endif ?>
 	<title><?php echo $title ?></title>
-<?php if( isset( $post ) && $post !== FALSE && $post->hasDescription() ): ?>
+<?php if( isset( $post ) && $post !== FALSE ): ?>
+<?php if( $post->hasDescription() ): ?>
 	<meta name="description" content="<?php echo addslashes( $post->getDescription() ) ?>" />
-<?php elseif( isset( $page ) && $page !== FALSE && $page->hasDescription() ): ?>
+<?php endif ?>
+<?php if( isset( $_GET['p'] ) ): ?>
+	<link rel="canonical" href="<?php echo $post->getLink() ?>" />
+<?php endif ?>
+<?php elseif( isset( $page ) && $page !== FALSE ): ?>
+<?php if( $page->hasDescription() ): ?>
 	<meta name="description" content="<?php echo addslashes( $page->getDescription() ) ?>" />
+<?php endif ?>
+<?php if( isset( $_GET['page'] ) ): ?>
+	<link rel="canonical" href="<?php echo $page->getLink() ?>" />
+<?php endif ?>
 <?php endif ?>
 	<link rel="icon" href="<?php echo THEME_PATH ?>img/favicon-192.png" type="image/png" sizes="192x192" />
 	<link rel="icon" href="<?php echo THEME_PATH ?>img/favicon-32.png" type="image/png" sizes="32x32" />
 	<link rel="stylesheet" href="<?php echo THEME_PATH ?>css/style.css?v=8" />
 	<link rel="stylesheet" href="<?php echo THEME_PATH ?>js/highlight/styles/stackoverflow-dark.min.css" />
-	<link rel="alternate" type="application/rss+xml" title="New posts (RSS)" href="<?php echo URL ?>feed/" />
+	<link rel="alternate" type="application/rss+xml" title="Posts on sebadorn.de (RSS)" href="<?php echo URL ?>feed/" />
 <?php
 
 $p = NULL;
